6dbb3cf726f1e0fad16ddedb00e126917c95ab0f
ChinecheremChidubem <chinecheremchidubem172@gmail.com>
2025-10-09 11:25:04 +0100

    Started Registration page

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,16 @@ import {
 } from "react-router-dom";
 import RootLayout from "./Layout/RootLayout";
 import CampusSearchResults from "./pages/CampusSearchResults";
+import CampusSignInUser from "./components/Registration/CampusSignInUser";
+import CampusSignInLandlord from "./components/Registration/CampusSignInLandlord"
+import CampusLoginPage from "./components/Registration/CapmusLoginPage"
 
 
 const App = () => {
   const router = createBrowserRouter(
       createRoutesFromElements(
         <Route path="/" element={<RootLayout />}>
-          <Route index element={<CampusSearchResults />} />
+          <Route index element={<CampusLoginPage />} />
           <Route path="about" element={<h1>About</h1>} />
           <Route path="contact" element={<h1>Contact</h1>} />
         </Route>
