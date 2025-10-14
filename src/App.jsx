7f9528ef3ed7d5e57d74b8d90a49535c9aa91f15
Allen Nwuba <nwubachukwuemelie@gmail.com>
2025-10-14 12:33:39 +0100

    removed usecontext for a while

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,18 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import RootLayout from "./Layout/RootLayout";
-import CampusSearchResults from "./pages/CampusSearchResults";
+// import CampusSearchResults from "./pages/CampusSearchResults";
+import CampusSpotListingForm from "./pages/CampusSpotListingForm";
+// import CampusSignInUser from "./components/Registration/CampusSignInUser";
+// import CampusSignInLandlord from "./components/Registration/CampusSignInLandlord"
+import CampusLoginPage from "./components/Registration/CapmusLoginPage";
+import CampusLandingPage from "./pages/CampusLandingPage.jsx";
+// import { createContext, useState } from "react";
+import CampusDashboard from "./pages/CampusDashboard.jsx";
 
+// Exporting the useContext
+// eslint-disable-next-line react-refresh/only-export-components
+// export const UserContext = createContext(null);
 
 const App = () => {
   // const [listings, setListings] = useState([]);
@@ -17,17 +27,30 @@ const App = () => {
   // };
 
   const router = createBrowserRouter(
-      createRoutesFromElements(
-        <Route path="/" element={<RootLayout />}>
-          <Route index element={<CampusSearchResults />} />
-          <Route path="about" element={<h1>About</h1>} />
-          <Route path="contact" element={<h1>Contact</h1>} />
-        </Route>
-      )
-    );
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<CampusSpotListingForm />} />
+        {/* <Route 
+          index element={<CampusLandingPage />} 
+        /> */}
+  
+        {/* <Route
+          index
+          path="/listingForm"
+          element={<CampusSpotListingForm onAddListing={handleAddListing} />}
+        />
+
+        <Route
+          path="/dashboard"
+          element={<CampusLandlordDashboard listings={listings} />}
+        />
+        <Route path="contact" element={<h1>Contact</h1>} /> */}
+      </Route>
+    )
+  );
   return (
     <div>
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
       {/* <UserContext.Provider>
       </UserContext.Provider> */}
     </div>
