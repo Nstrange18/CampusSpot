0671044cab60d8b3793454518bea7fd5d77042ee
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-10-14 11:19:48 +0100

    Set-up

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ const App = () => {
       createRoutesFromElements(
         <Route path="/" element={<RootLayout />}>
           <Route index element={<CampusLandingPage />} />
-          <Route path="about" element={<h1>About</h1>} />
-          <Route path="contact" element={<h1>Contact</h1>} />
+          {/* <Route path="about" element={<h1>About</h1>} /> */}
+          {/* <Route path="contact" element={<h1>Contact</h1>} /> */}
         </Route>
       )
     );
